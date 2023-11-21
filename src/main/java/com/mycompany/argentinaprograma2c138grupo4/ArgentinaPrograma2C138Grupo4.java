ee0ad24d34b0bb322da198f289bfbd06ae0fef95
Claudia Arias <claudiaarias76@hotmail.com>
2023-11-20 22:04:04 -0300

    clases

---
+++ src/main/java/com/mycompany/argentinaprograma2c138grupo4/ArgentinaPrograma2C138Grupo4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.argentinaprograma2c138grupo4;
+
+/**
+ *
+ * @author BANGHO
+ */
+@Data
+public class ArgentinaPrograma2C138Grupo4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/soporteOperativo/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soporteOperativo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author BANGHO
+ */
+@Data
+public class Cliente {
+    private String razonSocial;
+    private String cuit;
+}
